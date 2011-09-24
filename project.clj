89e4886f31352447183042ff97daad6eadccd1ac
Joost Diepenmaat <joost@zeekat.nl>
2011-09-24 09:53:08 +0200

    removed dev deps

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject zeekat-utils "0.1.0"
+(defproject zeekat-utils "0.1.1"
   :description "Generic utility functions"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+                 [org.clojure/clojure-contrib "1.2.0"]])
